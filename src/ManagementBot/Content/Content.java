08a6ff26f48005f1009f071e8549afdc66875c53
Strohgelaender <micky.321@web.de>
2017-03-31 13:21:34 +0200

    Changed Stats and added Welcome Message

--- src/ManagementBot/Content/Content.java
+++ src/ManagementBot/Content/Content.java
@@ -3,7 +3,7 @@ package ManagementBot.Content;
 import net.dv8tion.jda.core.EmbedBuilder;
 import net.dv8tion.jda.core.MessageBuilder;
 import net.dv8tion.jda.core.entities.*;
-import net.dv8tion.jda.core.entities.impl.MessageImpl;
+import net.dv8tion.jda.core.events.guild.member.GuildMemberJoinEvent;
 import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.core.exceptions.PermissionException;
 import org.json.JSONObject;
@@ -12,6 +12,8 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class Content {
     public static Message getStats() {
@@ -20,12 +22,14 @@ public class Content {
             String s = new BufferedReader(new InputStreamReader(url.openStream())).readLine();
             JSONObject jsonObject = new JSONObject(s).getJSONObject("game");
             MessageEmbed messageEmbed = new EmbedBuilder()
+                    .setTitle("Statistiken")
                     .addField("Blacklist-Einträge:", jsonObject.getInt("blacklists") + "", true)
                     .addField("Fehlgeschlagene Bot-Attaken:", jsonObject.getInt("bot_attacks_failed") +  "", true)
                     .addField("Verbindungen", jsonObject.getInt("connections_to_target") + "", true)
                     .addField("Erfolgreich geknackte Passwörter:", jsonObject.getInt("successful_cracked_passwords") + "", true)
                     .addField("Gestohlene Miner", jsonObject.getInt("total_miners_stolen") + "", true)
                     .addField("Gestohlene Wallets", jsonObject.getInt("total_wallets_stolen") + "", true)
+                    .setFooter("Stand: " + new Date(), "https://avatars0.githubusercontent.com/u/26769965?v=3&s=200")
                     .build();
             return new MessageBuilder().setEmbed(messageEmbed).build();
         } catch (IOException e) {
@@ -56,6 +60,14 @@ public class Content {
         } catch (PermissionException e) {
             e.printStackTrace();
         }
+    }
+    public static Message getJoinMessage(GuildMemberJoinEvent event) {
+        return new MessageBuilder().append("Willkommen, ")
+                .append(event.getMember())
+                .append(". Bevor du uns Fragen vorlegst, wirf bitte unbedingt zunächst einen Blick in die ")
+                .append(event.getGuild().getTextChannelsByName("regeln", true).get(0))
+                .append("\nViel Spaß bei der GHC | German Hackers Community")
+                .build();
     }
 
 }
--- src/ManagementBot/Listener/JoinListener.java
+++ src/ManagementBot/Listener/JoinListener.java
@@ -12,12 +12,7 @@ public class JoinListener extends ListenerAdapter{
     @Override
     public void onGuildMemberJoin(GuildMemberJoinEvent event) {
         TextChannel gerneral = event.getGuild().getTextChannelsByName("justcodingthings", true).get(0);
-        gerneral.sendMessage(new MessageBuilder().append("Willkommen, ")
-                .append(event.getMember())
-                .append(". Bevor du uns Fragen vorlegst, wirf bitte unbedingt zunächst einen Blick in die ")
-                .append(event.getGuild().getTextChannelsByName("regeln", true).get(0))
-                .append("\nViel Spaß bei der GHC | German Hackers Community")
-                .build()).queue();
+        gerneral.sendMessage(Content.getJoinMessage(event)).queue();
         Content.addRole(event.getMember(), event.getGuild(), event.getGuild().getRolesByName("Neuling", true).get(0));
 
     }

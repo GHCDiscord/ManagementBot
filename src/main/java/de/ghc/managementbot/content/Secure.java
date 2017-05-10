d96f36b660eefc9b8441c774b744be71d0c18909
Strohgelaender <micky.321@web.de>
2017-05-10 16:17:32 +0200

    Secure 1

---
+++ src/main/java/de/ghc/managementbot/content/Secure.java
@@ -0,0 +1,10 @@
+package de.ghc.managementbot.content;
+
+public class Secure {
+    public static final String ConsumerKey = "";
+    public static final String ConsumerSecret = "";
+    public static final String AccessToken = "";
+    public static final String AccesSecret = "";
+    public static final String DBToken = "";
+    public static final String DiscordToken = "";
+}

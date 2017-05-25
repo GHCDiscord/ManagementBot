58470897d2b12336d26fcc319cc2cde3c98d79c3
Strohgelaender <micky.321@web.de>
2017-05-25 14:11:49 +0200

    Bot CI

--- src/main/java/de/ghc/managementbot/commands/AddUser.java
+++ src/main/java/de/ghc/managementbot/commands/AddUser.java
@@ -2,6 +2,8 @@ package de.ghc.managementbot.commands;
 
 import de.ghc.managementbot.content.Content;
 import de.ghc.managementbot.content.Database;
+import de.ghc.managementbot.content.Strings;
+import de.ghc.managementbot.entity.Command;
 import de.ghc.managementbot.entity.User;
 import de.ghc.managementbot.threads.DeleteMessageThread;
 import net.dv8tion.jda.core.entities.ChannelType;
@@ -25,14 +27,22 @@ public class AddUser extends Database implements Command {
       String[] usernamearr = event.getMessage().getContent().split(" ");
       if (usernamearr.length > 2) {
         event.getAuthor().openPrivateChannel().queue(DM -> {
-          DM.sendMessage("Bei Nutzernamen in der Datenbank ist nur ein Wort erlaubt (keine Leerzeichen).\nDein Account wird nun mit dem ersten Wort deines Nutzernamens erstellt").queue();
+          //DM.sendMessage("Bei Nutzernamen in der Datenbank ist nur ein Wort erlaubt (keine Leerzeichen).\nDein Account wird nun mit dem ersten Wort deines Nutzernamens erstellt").queue();
+          DM.sendMessage(Strings.getString(Strings.register_error_multipleWords)).queue();
         });
-      } else if (usernamearr.length < 1) {
-        event.getAuthor().openPrivateChannel().queue(DM -> DM.sendMessage("Bitte gebe einen Nutzernamen f\u00FCr die Datenbank an!").queue(m -> new Thread(new DeleteMessageThread(120, m)).start()));
+      } else if (usernamearr.length < 2) {
+        event.getAuthor().openPrivateChannel().queue(DM -> {
+          //DM.sendMessage("Bitte gebe einen Nutzernamen f\u00FCr die Datenbank an!").queue(m -> new Thread(new DeleteMessageThread(120, m)).start());
+          DM.sendMessage(Strings.getString(Strings.register_error_noUsername)).queue();
+        });
+        return;
       }
       String username = usernamearr[1];
       if (!username.matches("[a-zA-Z0-9_]*")) {
-        event.getAuthor().openPrivateChannel().queue(DM -> DM.sendMessage("Im Nutzernamen sind nur A-Z, a-z, 0-9 und _ erlaubt!").queue());
+        event.getAuthor().openPrivateChannel().queue(DM -> {
+          //DM.sendMessage("Im Nutzernamen sind nur A-Z, a-z, 0-9 und _ erlaubt!").queue();
+          DM.sendMessage(Strings.getString(Strings.register_error_unexpectedChars)).queue();
+        });
         return;
       }
       User user = new User(username);
@@ -42,18 +52,21 @@ public class AddUser extends Database implements Command {
         String password = user.getPassword();
         if (response.equals("success")) {
           event.getAuthor().openPrivateChannel().queue(DM -> {
-            DM.sendMessage(String.format("Dein Account wurde erfolgreich erstellt.\nNutzername: %s\nPasswort: %s\nDein Account ist nun 30 Tage g\u00FCltig. Danach musst du mit `!refresh` deinen Account reaktivieren.\nViel Spaß mit der IP-Datenbank unter %s", username, password, url)).queue();
+            //DM.sendMessage(String.format("Dein Account wurde erfolgreich erstellt.\nNutzername: %s\nPasswort: %s\nDein Account ist nun 30 Tage g\u00FCltig. Danach musst du mit `!refresh` deinen Account reaktivieren.\nViel Spaß mit der IP-Datenbank unter %s", username, password, url)).queue();
+            DM.sendMessage(Strings.getString(Strings.register_success_addedAccount).replace("$[name]", username).replace("$[password]", password).replace("$[[url]", url)).queue();
           });
         } else if (response.equals("discord taken")) {
           event.getAuthor().openPrivateChannel().queue(DM -> {
-            DM.sendMessage("Du hast bereits einen Account in der Datenbank. Reaktivieren kannst du diesen mit `!refresh`").queue(m -> new Thread(new DeleteMessageThread(120, m)).start());
+            //DM.sendMessage("Du hast bereits einen Account in der Datenbank. Reaktivieren kannst du diesen mit `!refresh`").queue(m -> new Thread(new DeleteMessageThread(120, m)).start());
+            DM.sendMessage(Strings.getString(Strings.register_error_accountAlreadyExists)).queue();
           });
         } else if (response.equals("name taken")) {
-          event.getAuthor().openPrivateChannel().queue(DM ->
-              DM.sendMessage(String.format("Der Nutzername %s ist bereits vergeben! Bitte w\u00E4hle einen anderen Nutzernamen!", username)).queue(m -> new Thread(new DeleteMessageThread(120, m)).start())
-          );
+          event.getAuthor().openPrivateChannel().queue(DM -> {
+            //DM.sendMessage(String.format("Der Nutzername %s ist bereits vergeben! Bitte w\u00E4hle einen anderen Nutzernamen!", username)).queue(m -> new Thread(new DeleteMessageThread(120, m)).start());
+            DM.sendMessage(Strings.getString(Strings.register_error_usernameIsAlreadyTaken).replace("$[name]", username)).queue(); //TODO replacement
+          });
         } else {
-          event.getAuthor().openPrivateChannel().queue(DM -> DM.sendMessage("Es ist ein unerwarteter Fehler aufgetreten! Bitte sende die folgenden Informationen an die Programmierer via @Coding:\n" + response).queue());
+          event.getAuthor().openPrivateChannel().queue(DM -> DM.sendMessage(Strings.getString(Strings.addIP_error_exception).replace("$[err]", response)).queue());
         }
       }).start();
     }

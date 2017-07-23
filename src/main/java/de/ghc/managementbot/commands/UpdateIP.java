ae411074a829c1fedaade9db3558b6de23902e10
Strohgelaender <micky.321@web.de>
2017-07-23 13:09:18 +0200

    UpdateIP

---
+++ src/main/java/de/ghc/managementbot/commands/UpdateIP.java
@@ -0,0 +1,26 @@
+package de.ghc.managementbot.commands;
+
+import de.ghc.managementbot.content.Content;
+import de.ghc.managementbot.content.Data;
+import de.ghc.managementbot.entity.Command;
+import de.ghc.managementbot.entity.IPEntry;
+import de.ghc.managementbot.threads.DeleteMessageThread;
+import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
+
+public class UpdateIP implements Command {
+    private IPEntry entry;
+
+    public UpdateIP(IPEntry entry) {
+        this.entry = entry;
+    }
+
+    @Override
+    public void onMessageReceived(MessageReceivedEvent event) {
+        String msg = event.getMessage().getContent();
+        new Thread(new DeleteMessageThread(60, event.getMessage())).start();
+        if (msg.equalsIgnoreCase("ja") || msg.equalsIgnoreCase("j") || msg.equalsIgnoreCase("y")) {
+            Content.getGhc().getTextChannelById(Data.zahlenschlacht).sendMessage(entry.toString()).queue();
+        }
+        Content.deleteUserUpdateIP(event.getAuthor(), this);
+    }
+}
